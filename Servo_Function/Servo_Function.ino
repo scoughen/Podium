70d59c680cec15d265f447cc4d15d28fccd8838c
mmesward <60489696+mmesward@users.noreply.github.com>
2020-02-24 10:16:43 -0700

    Update Servo_Function.ino

--- Servo_Function/Servo_Function.ino
+++ Servo_Function/Servo_Function.ino
@@ -1,39 +1,68 @@
 #include <Servo.h>
 
 Servo servo_1;  //Create servo object to control first servo
+Servo servo_2;  //Create servo object to control second servo
+Servo servo_3;  //Create servo object to control third servo
+Servo servo_4;  //Create servo object to control fourth servo
+Servo servo_5;  //Create servo object to control fith servo
+Servo servo_6;  //Create servo object to control sixth servo
+Servo servo_7;  //Create servo object to control seventh servo
+Servo servo_8;  //Create servo object to control eighth servo
+Servo servo_9;  //Create servo object to control nineth servo
+Servo servo_10;  //Create servo object to control tenth servo
+
 
 int pos = 0;  //Integer value for servo position
 
-int potpin = 0;  //potentiometrer attached to analog pin zero
+int potpin1 = 0;  //potentiometrer attached to analog pin zero
+int potpin2 = 0;  //potentiometrer attached to analog pin zero
+int potpin3 = 0;  //potentiometrer attached to analog pin zero
+int potpin4 = 0;  //potentiometrer attached to analog pin zero
+int potpin5 = 0;  //potentiometrer attached to analog pin zero
+int potpin6 = 0;  //potentiometrer attached to analog pin zero
+int potpin7 = 0;  //potentiometrer attached to analog pin zero
+int potpin8 = 0;  //potentiometrer attached to analog pin zero
+int potpin9 = 0;  //potentiometrer attached to analog pin zero
+int potpin10 = 0;  //potentiometrer attached to analog pin zero
 int val;  //variable to read value from the analogue pin
 
 
 void setup() {
   // put your setup code here, to run once:
-servo_1.attach();  //Servo 1 is attached to pin
-servo_2.attach();  //Servo 2 is attached to pin 
-servo_3.attach();  //Servo 3 is attached to pin
-servo_4.attach();  //Servo 4 is attached to pin
-servo_5.attach();  //Servo 5 is attached to pin
-servo_6.attach();  //Servo 6 is attached to pin
-servo_7.attach();  //Servo 7 is attached to pin
-servo_8.attach();  //Servo 8 is attached to pin
-servo_9.attach();  //Servo 9 is attached to pin
-servo_10.attach();  //Servo 10 is attached to pin
+servo_1.attach(?);  //Servo 1 is attached to pin
+servo_2.attach(?);  //Servo 2 is attached to pin 
+servo_3.attach(?);  //Servo 3 is attached to pin
+servo_4.attach(?);  //Servo 4 is attached to pin
+servo_5.attach(?);  //Servo 5 is attached to pin
+servo_6.attach(?);  //Servo 6 is attached to pin
+servo_7.attach(?);  //Servo 7 is attached to pin
+servo_8.attach(?);  //Servo 8 is attached to pin
+servo_9.attach(?);  //Servo 9 is attached to pin
+servo_10.attach(?);  //Servo 10 is attached to pin
 }
-
 
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+servo_control_1();
+servo_control_2();
+servo_control_3();
+servo_control_4();
+servo_control_5();
+servo_control_6();
+servo_control_7();
+servo_control_8();
+servo_control_9();
+servo_control_10();
 }
+
 
 //Servo 1 Function
 float servo_control_1(){
-int val = analogRead(potpin);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);
-if ((val >=0) && (val <36)){  //
+int val = analogRead(potpin1);  //Reads value of potentiometer (value between 0 and 1023)
+val = map(val,0,1023,0,180); //Converts 0-1023 range to 0-180
+if ((val >=0) && (val <36)){  
   servo_1.write(18);
   delay(15);     
 }
@@ -55,9 +84,9 @@ if ((val >=144) && (val <=180)){
 
 //Servo 2 Function
 float servo_control_2(){
-int val = analogRead(potpin);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);
-if ((val >=0) && (val <36)){  //
+int val = analogRead(potpin2);  //Reads value of potentiometer (value between 0 and 1023)
+val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+if ((val >=0) && (val <36)){  
   servo_1.write(18);
   delay(15);     
 }
@@ -79,9 +108,9 @@ if ((val >=144) && (val <=180)){
 
 //Servo 3 Function
 float servo_control_3(){
-int val = analogRead(potpin);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);
-if ((val >=0) && (val <36)){  //
+int val = analogRead(potpin3);  //Reads value of potentiometer (value between 0 and 1023)
+val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+if ((val >=0) && (val <36)){  
   servo_3.write(18);
   delay(15);     
 }
@@ -103,9 +132,9 @@ if ((val >=144) && (val <=180)){
 
 //servo 4 Function
 float servo_control_3(){
-int val = analogRead(potpin);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);
-if ((val >=0) && (val <36)){  //
+int val = analogRead(potpin4);  //Reads value of potentiometer (value between 0 and 1023)
+val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+if ((val >=0) && (val <36)){  
   servo_3.write(18);
   delay(15);     
 }
@@ -126,9 +155,9 @@ if ((val >=144) && (val <=180)){
 }
 //Servo 5 Function
 float servo_control_5(){
-int val = analogRead(potpin);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);
-if ((val >=0) && (val <36)){  //
+int val = analogRead(potpin5);  //Reads value of potentiometer (value between 0 and 1023)
+val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+if ((val >=0) && (val <36)){  
   servo_5.write(18);
   delay(15);     
 }
@@ -150,9 +179,9 @@ if ((val >=144) && (val <=180)){
 
 //Servo 6 function
 float servo_control_6(){
-int val = analogRead(potpin);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);
-if ((val >=0) && (val <36)){  //
+int val = analogRead(potpin6);  //Reads value of potentiometer (value between 0 and 1023)
+val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+if ((val >=0) && (val <36)){  
   servo_6.write(18);
   delay(15);     
 }
@@ -174,9 +203,9 @@ if ((val >=144) && (val <=180)){
 
 //Servo 7 Function
 float servo_control_7(){
-int val = analogRead(potpin);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);
-if ((val >=0) && (val <36)){  //
+int val = analogRead(potpin7);  //Reads value of potentiometer (value between 0 and 1023)
+val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+if ((val >=0) && (val <36)){  
   servo_7.write(18);
   delay(15);     
 }
@@ -198,9 +227,9 @@ if ((val >=144) && (val <=180)){
 
 //Servo 8 Function
 float servo_control_8(){
-int val = analogRead(potpin);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);
-if ((val >=0) && (val <36)){  //
+int val = analogRead(potpin8);  //Reads value of potentiometer (value between 0 and 1023)
+val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+if ((val >=0) && (val <36)){  
   servo_8.write(18);
   delay(15);     
 }
@@ -222,9 +251,9 @@ if ((val >=144) && (val <=180)){
 
 //Servo 9 Function
 float servo_control_9(){
-int val = analogRead(potpin);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);
-if ((val >=0) && (val <36)){  //
+int val = analogRead(potpin9);  //Reads value of potentiometer (value between 0 and 1023)
+val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+if ((val >=0) && (val <36)){  
   servo_9.write(18);
   delay(15);     
 }
@@ -246,9 +275,9 @@ if ((val >=144) && (val <=180)){
 
 //Servo 10 Function
 float servo_control_10(){
-int val = analogRead(potpin);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);
-if ((val >=0) && (val <36)){  //
+int val = analogRead(potpin10);  //Reads value of potentiometer (value between 0 and 1023)
+val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+if ((val >=0) && (val <36)){  
   servo_10.write(18);
   delay(15);     
 }
