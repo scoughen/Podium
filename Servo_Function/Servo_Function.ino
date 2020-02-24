4decdec6dd7addb433411463a241c70ce6e4a381
mmesward <60489696+mmesward@users.noreply.github.com>
2020-02-24 10:33:00 -0700

    Update Servo_Function.ino

--- Servo_Function/Servo_Function.ino
+++ Servo_Function/Servo_Function.ino
@@ -60,261 +60,260 @@ servo_control_10();
 
 //Servo 1 Function
 float servo_control_1(){
-int val = analogRead(potpin1);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180); //Converts 0-1023 range to 0-180
-if ((val >=0) && (val <36)){  
-  servo_1.write(18);
-  delay(15);     
-}
-if ((val >=36) && (val <72)){
-   servo_1.write(54);
-   delay(15);
-}
-if ((val >=72) && (val <108)){
-  servo_1.write(90);
-  delay(15);
-}
-if ((val >=108) && (val <144)){
-  servo_1.write(126);
-  delay(15);
-}
-if ((val >=144) && (val <=180)){
-  servo_1.write(162);
+  int val = analogRead(potpin1);  //Reads value of potentiometer (value between 0 and 1023)
+  val = map(val,0,1023,0,180); //Converts 0-1023 range to 0-180
+  if ((val >=0) && (val <36)){  
+    servo_1.write(18);
+    delay(15);     
+  }
+  if ((val >=36) && (val <72)){
+     servo_1.write(54);
+     delay(15);
+  }
+  if ((val >=72) && (val <108)){
+    servo_1.write(90);
+    delay(15);
+  }
+  if ((val >=108) && (val <144)){
+    servo_1.write(126);
+    delay(15);
+  }
+  if ((val >=144) && (val <=180)){
+    servo_1.write(162);
+  }
 return val;  
-}
 
 }
 //Servo 2 Function
 float servo_control_2(){
-int val = analogRead(potpin2);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
-if ((val >=0) && (val <36)){  
-  servo_1.write(18);
-  delay(15);     
-}
-if ((val >=36) && (val <72)){
-   servo_2.write(54);
-   delay(15);
-}
-if ((val >=72) && (val <108)){
-  servo_2.write(90);
-  delay(15);
-}
-if ((val >=108) && (val <144)){
-  servo_2.write(126);
-  delay(15);
-}
-if ((val >=144) && (val <=180)){
-  servo_2.write(162);
+  int val = analogRead(potpin2);  //Reads value of potentiometer (value between 0 and 1023)
+  val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+  if ((val >=0) && (val <36)){  
+    servo_1.write(18);
+    delay(15);     
+  }
+  if ((val >=36) && (val <72)){
+     servo_2.write(54);
+     delay(15);
+  }
+  if ((val >=72) && (val <108)){
+    servo_2.write(90);
+    delay(15);
+  }
+  if ((val >=108) && (val <144)){
+    servo_2.write(126);
+    delay(15);
+  }
+  if ((val >=144) && (val <=180)){
+    servo_2.write(162);
+  }
 return val;
-}
 }
 
 //Servo 3 Function
 float servo_control_3(){
-int val = analogRead(potpin3);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
-if ((val >=0) && (val <36)){  
-  servo_3.write(18);
-  delay(15);     
-}
-if ((val >=36) && (val <72)){
-   servo_3.write(54);
-   delay(15);
-}
-if ((val >=72) && (val <108)){
-  servo_3.write(90);
-  delay(15);
-}
-if ((val >=108) && (val <144)){
-  servo_3.write(126);
-  delay(15);
-}
-if ((val >=144) && (val <=180)){
-  servo_3.write(162);
+  int val = analogRead(potpin3);  //Reads value of potentiometer (value between 0 and 1023)
+  val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+  if ((val >=0) && (val <36)){  
+    servo_3.write(18);
+    delay(15);     
+  }
+  if ((val >=36) && (val <72)){
+     servo_3.write(54);
+     delay(15);
+  }
+  if ((val >=72) && (val <108)){
+    servo_3.write(90);
+    delay(15);
+  }
+  if ((val >=108) && (val <144)){
+    servo_3.write(126);
+    delay(15);
+  }
+  if ((val >=144) && (val <=180)){
+    servo_3.write(162);
+  }
 return val;
-}
 }
 
 //servo 4 Function
 float servo_control_3(){
-int val = analogRead(potpin4);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
-if ((val >=0) && (val <36)){  
-  servo_3.write(18);
-  delay(15);     
-}
-if ((val >=36) && (val <72)){
-   servo_3.write(54);
-   delay(15);
-}
-if ((val >=72) && (val <108)){
-  servo_3.write(90);
-  delay(15);
-}
-if ((val >=108) && (val <144)){
-  servo_3.write(126);
-  delay(15);
-}
-if ((val >=144) && (val <=180)){
-  servo_3.write(162);
+  int val = analogRead(potpin4);  //Reads value of potentiometer (value between 0 and 1023)
+  val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+  if ((val >=0) && (val <36)){  
+    servo_3.write(18);
+    delay(15);     
+  }
+  if ((val >=36) && (val <72)){
+     servo_3.write(54);
+     delay(15);
+  }
+  if ((val >=72) && (val <108)){
+    servo_3.write(90);
+    delay(15);
+  }
+  if ((val >=108) && (val <144)){
+    servo_3.write(126);
+    delay(15);
+  }
+  if ((val >=144) && (val <=180)){
+    servo_3.write(162);
+  }
 return val;
-}
 }
 
 //Servo 5 Function
 float servo_control_5(){
-int val = analogRead(potpin5);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
-if ((val >=0) && (val <36)){  
-  servo_5.write(18);
-  delay(15);     
-}
-if ((val >=36) && (val <72)){
-   servo_5.write(54);
-   delay(15);
-}
-if ((val >=72) && (val <108)){
-  servo_5.write(90);
-  delay(15);
-}
-if ((val >=108) && (val <144)){
-  servo_5.write(126);
-  delay(15);
-}
-if ((val >=144) && (val <=180)){
-  servo_5.write(162);
+  int val = analogRead(potpin5);  //Reads value of potentiometer (value between 0 and 1023)
+  val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+  if ((val >=0) && (val <36)){  
+    servo_5.write(18);
+    delay(15);     
+  }
+  if ((val >=36) && (val <72)){
+     servo_5.write(54);
+     delay(15);
+  }
+  if ((val >=72) && (val <108)){
+    servo_5.write(90);
+    delay(15);
+  }
+  if ((val >=108) && (val <144)){
+    servo_5.write(126);
+    delay(15);
+  }
+  if ((val >=144) && (val <=180)){
+    servo_5.write(162);
+  }
 return val;
-}
 }
 
 //Servo 6 function
 float servo_control_6(){
-int val = analogRead(potpin6);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
-if ((val >=0) && (val <36)){  
-  servo_6.write(18);
-  delay(15);     
-}
-if ((val >=36) && (val <72)){
-   servo_6.write(54);
-   delay(15);
-}
-if ((val >=72) && (val <108)){
-  servo_6.write(90);
-  delay(15);
-}
-if ((val >=108) && (val <144)){
-  servo_6.write(126);
-  delay(15);
-}
-if ((val >=144) && (val <=180)){
-  servo_6.write(162);
+  int val = analogRead(potpin6);  //Reads value of potentiometer (value between 0 and 1023)
+  val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+  if ((val >=0) && (val <36)){  
+    servo_6.write(18);
+    delay(15);     
+  }
+  if ((val >=36) && (val <72)){
+     servo_6.write(54);
+     delay(15);
+  }
+  if ((val >=72) && (val <108)){
+    servo_6.write(90);
+    delay(15);
+  }
+  if ((val >=108) && (val <144)){
+    servo_6.write(126);
+    delay(15);
+  }
+  if ((val >=144) && (val <=180)){
+    servo_6.write(162);
+  }
 return val;
-}
 }
 
 //Servo 7 Function
 float servo_control_7(){
-int val = analogRead(potpin7);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
-if ((val >=0) && (val <36)){  
-  servo_7.write(18);
-  delay(15);     
-}
-if ((val >=36) && (val <72)){
-   servo_7.write(54);
-   delay(15);
-}
-if ((val >=72) && (val <108)){
-  servo_7.write(90);
-  delay(15);
-}
-if ((val >=108) && (val <144)){
-  servo_7.write(126);
-  delay(15);
-}
-if ((val >=144) && (val <=180)){
-  servo_7.write(162);
+    int val = analogRead(potpin7);  //Reads value of potentiometer (value between 0 and 1023)
+    val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+    if ((val >=0) && (val <36)){  
+      servo_7.write(18);
+      delay(15);     
+    }
+    if ((val >=36) && (val <72)){
+       servo_7.write(54);
+       delay(15);
+    }
+    if ((val >=72) && (val <108)){
+      servo_7.write(90);
+      delay(15);
+    }
+    if ((val >=108) && (val <144)){
+      servo_7.write(126);
+      delay(15);
+    }
+    if ((val >=144) && (val <=180)){
+      servo_7.write(162);
+    }
 return val;
-}
 }
 
 //Servo 8 Function
 float servo_control_8(){
-int val = analogRead(potpin8);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
-if ((val >=0) && (val <36)){  
-  servo_8.write(18);
-  delay(15);     
-}
-if ((val >=36) && (val <72)){
-   servo_8.write(54);
-   delay(15);
-}
-if ((val >=72) && (val <108)){
-  servo_8.write(90);
-  delay(15);
-}
-if ((val >=108) && (val <144)){
-  servo_8.write(126);
-  delay(15);
-}
-if ((val >=144) && (val <=180)){
-  servo_8.write(162);
+  int val = analogRead(potpin8);  //Reads value of potentiometer (value between 0 and 1023)
+  val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+  if ((val >=0) && (val <36)){  
+    servo_8.write(18);
+    delay(15);     
+  }
+  if ((val >=36) && (val <72)){
+     servo_8.write(54);
+     delay(15);
+  }
+  if ((val >=72) && (val <108)){
+    servo_8.write(90);
+    delay(15);
+  }
+  if ((val >=108) && (val <144)){
+    servo_8.write(126);
+    delay(15);
+  }
+  if ((val >=144) && (val <=180)){
+    servo_8.write(162);
+  }
 return val;
-}
 }
 
 //Servo 9 Function
 float servo_control_9(){
-int val = analogRead(potpin9);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
-if ((val >=0) && (val <36)){  
-  servo_9.write(18);
-  delay(15);     
-}
-if ((val >=36) && (val <72)){
-   servo_9.write(54);
-   delay(15);
-}
-if ((val >=72) && (val <108)){
-  servo_9.write(90);
-  delay(15);
-}
-if ((val >=108) && (val <144)){
-  servo_9.write(126);
-  delay(15);
-}
-if ((val >=144) && (val <=180)){
-  servo_9.write(162);
+  int val = analogRead(potpin9);  //Reads value of potentiometer (value between 0 and 1023)
+  val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+  if ((val >=0) && (val <36)){  
+    servo_9.write(18);
+    delay(15);     
+  }
+  if ((val >=36) && (val <72)){
+     servo_9.write(54);
+     delay(15);
+  }
+  if ((val >=72) && (val <108)){
+    servo_9.write(90);
+    delay(15);
+  }
+  if ((val >=108) && (val <144)){
+    servo_9.write(126);
+    delay(15);
+  }
+  if ((val >=144) && (val <=180)){
+    servo_9.write(162);
+  }
 return val;
-}
 }
 
 //Servo 10 Function
 float servo_control_10(){
-int val = analogRead(potpin10);  //Reads value of potentiometer (value between 0 and 1023)
-val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
-if ((val >=0) && (val <36)){  
-  servo_10.write(18);
-  delay(15);     
+  int val = analogRead(potpin10);  //Reads value of potentiometer (value between 0 and 1023)
+  val = map(val,0,1023,0,180);  //Converts 0-1023 range to 0-180
+  if ((val >=0) && (val <36)){  
+    servo_10.write(18);
+    delay(15);     
+  }
+  if ((val >=36) && (val <72)){
+     servo_10.write(54);
+     delay(15);
+  }
+  if ((val >=72) && (val <108)){
+    servo_10.write(90);
+    delay(15);
+  }
+  if ((val >=108) && (val <144)){
+    servo_10.write(126);
+    delay(15);
+  }
+  if ((val >=144) && (val <=180)){
+    servo_10.write(162);
 }
-if ((val >=36) && (val <72)){
-   servo_10.write(54);
-   delay(15);
-}
-if ((val >=72) && (val <108)){
-  servo_10.write(90);
-  delay(15);
-}
-if ((val >=108) && (val <144)){
-  servo_10.write(126);
-  delay(15);
-}
-if ((val >=144) && (val <=180)){
-  servo_10.write(162);
-}
-
-
+retun val;
 }
