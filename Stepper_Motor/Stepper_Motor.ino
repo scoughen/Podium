38aa9673739186c5474e55901d3e453aef6c6dee
mmesward <60489696+mmesward@users.noreply.github.com>
2020-03-02 10:20:11 -0700

    Update Stepper_Motor.ino

--- Stepper_Motor/Stepper_Motor.ino
+++ Stepper_Motor/Stepper_Motor.ino
@@ -1,9 +1,8 @@
 #include <Stepper.h>
-
-const int stepsPerRevolution = 200;  // Steps per revolution of the motor
+#define stepsPerRev 200;  // Steps per revolution of the motor
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+Stepper myStepper(stepsPerRev, 8, 9, 10, 11);
 
 void setup() {
   myStepper.setSpeed(60);  // sets the speed at 60 rpm
@@ -11,7 +10,11 @@ void setup() {
 }
 
 void loop() {
-  myStepper.step(10);  // steps taken 
+  myStepper.step(10);  // steps taken
   delay(1000);  // wait one second before repeating motion
 
 }
+
+
+// Change stepsPerRev to our motor (can be modified with gear reduction for more steps)
+// Need to determine how many steps necessary to rotate barrel to each cutout
