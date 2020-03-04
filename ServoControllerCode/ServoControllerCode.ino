2f9a232a29b29714528c3f80c03d89f12fe08432
scoughen <60477261+scoughen@users.noreply.github.com>
2020-03-04 13:21:39 -0700

    Update ServoControllerCode.ino

--- ServoControllerCode/ServoControllerCode.ino
+++ ServoControllerCode/ServoControllerCode.ino
@@ -1,12 +1,32 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <Wire.h>
 
-void setup() {
-  // put your setup code here, to run once:
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
+#define USMIN  1000 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX  2000 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define servonum  0 // servo being driven
+
+void setup() {
+  Serial.begin(9600);
+
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+
+  delay(10);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (uint16_t microsec = USMIN; microsec < USMAX; microsec=microsec+10) {
+    pwm.writeMicroseconds(servonum, microsec);
+  }
 
+  delay(500);
+  for (uint16_t microsec = USMAX; microsec > USMIN; microsec=microsec-10) {
+    pwm.writeMicroseconds(servonum, microsec);
+  }
+  
+  delay(500);
 }
