08b33603972d3ab8da4f8cb6c0f9fff185c717f3
scoughen <60477261+scoughen@users.noreply.github.com>
2020-03-27 11:35:08 -0600

    Update MainCode.ino

--- MainCode/MainCode.ino
+++ MainCode/MainCode.ino
@@ -1,46 +1,38 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <Wire.h>
-#include <Stepper.h>
 
+//------User Controlled Values
+  //Weight Thresholds
+    #define VeryLow 0.500 // (g)
+    #define Low 0.525 // (g)
+    #define High 0.535 // (g)
+    #define VeryHigh 0.550 // (g)
+//------END OF USER CONTROLLED VALUES
 
-// initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRev, 8, 9, 10, 11);
-#define stepsPerRev 200  // Steps per revolution of Cogwheel stepper motor
+#define scaleTime 1100  //time spent weighing each pellet in miliseconds
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 #define USMIN  1000 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
 #define USMAX  1500 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
-#define servo0  0 // servo being driven
-#define servo1  1 // servo being driven
-#define servo2  2 // servo being driven
-#define servo3  3 // servo being driven
-#define servo4  4 // servo being driven
-#define servo5  5 // servo being driven
-#define servo6  6 // servo being driven
-#define servo7  7 // servo being driven
-#define servo8  8 // servo being driven
-#define servo9  9 // servo being driven
 
 float scale[9];  //values for current readings from all scales
 
-int pos = 0;  //Integer value for servo position
+//--------Stepper Motor Var's
+#define StepsPerIncrement 24
+int pulse = 7; //pulse pin
+int dir = 6; // direction pin
+int en = 5; // enable pin
 
-int potpin1 = 0;  //potentiometrer attached to analog pin zero
-int potpin2 = 0;  //potentiometrer attached to analog pin zero
-int potpin3 = 0;  //potentiometrer attached to analog pin zero
-int potpin4 = 0;  //potentiometrer attached to analog pin zero
-int potpin5 = 0;  //potentiometrer attached to analog pin zero
-int potpin6 = 0;  //potentiometrer attached to analog pin zero
-int potpin7 = 0;  //potentiometrer attached to analog pin zero
-int potpin8 = 0;  //potentiometrer attached to analog pin zero
-int potpin9 = 0;  //potentiometrer attached to analog pin zero
-int potpin10 = 0;  //potentiometrer attached to analog pin zero
-int val;  //variable to read value from the analogue pin
+//--------Scale Calibration Var's
+float ReadingA_Strain[9];
+float LoadA_Strain[9]; 
+float ReadingB_Strain[9];
+float LoadB_Strain[9];
 
 //--------Selector Interrupt Var's
-const byte selectorInterruptPin = 2;
+const byte SelectorInterruptPin = 2;
 int sel1flg = 0;
 
 //--------Funnel Interrupt Var's
@@ -59,6 +51,19 @@ bool step4[9]; //1 element per line  -- value for pellet presence when reaches s
 //--------
 
 void setup() {
+  //--------Scale Calibration Data
+    //--------Scale 0
+      ReadingA_Strain[0] = 755.0;
+      LoadA_Strain[0] = 0.0; //  (g) 
+      ReadingB_Strain[0] = 781.0;
+      LoadB_Strain[0] = 2.9; //  (g) 
+    //--------Scale 1
+      ReadingA_Strain[1] = 866.0;
+      LoadA_Strain[1] = 0.0; //  (g) 
+      ReadingB_Strain[1] = 947.0;
+      LoadB_Strain[1] = 2.9; //  (g) 
+    //--------Scale etc.......................................
+
   //------Servo setup
   Serial.begin(9600);
   pwm.begin();
@@ -66,10 +71,17 @@ void setup() {
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
   delay(10);
 
+  //------Set up for Stepper Motor
+  pinMode (pulse, OUTPUT);
+  pinMode (dir, OUTPUT);
+  pinMode (en, OUTPUT);
+
+  //------Set up for Reading Scales
+  analogReference(INTERNAL1V1);
+
   //------Set up for Selector Interrupt
-  pinMode(ledPin, OUTPUT);
-  pinMode(statPin, OUTPUT);
-  pinMode(interruptPin, INPUT_PULLUP);
+  pinMode(SelectorInterruptPin, INPUT_PULLUP);
+  int sel1flg;
   
   attachInterrupt(digitalPinToInterrupt(SelectorInterruptPin), Selector, RISING);
   //------Set up for Funnel Interrupt
@@ -79,22 +91,25 @@ void setup() {
   //------Set up for Cogwheel Error Checking
   pinMode(inlet0, INPUT);
   pinMode(scale0, INPUT);
-  
-  //------Set up for Cogwheel Stepper Motor
-  myStepper.setSpeed(60);  // sets the speed at 60 rpm
-  Serial.begin(9600);  // serial port
 }
 
 
 void loop() {
-  for (int i = 0; i<=9; i++)
+  for (int i = 0; i<=0; i++)
   {
     scale[i] = servo_control(i);
   }
 
   //-------Cogwheel Control
-  myStepper.step(10);  // steps taken
-//  delay(1000); <-----------------------DO WE NEED THIS? Settling time for load cells?
+  digitalWrite(dir, LOW);
+  digitalWrite(en, HIGH);
+  
+  for (int i=0; i <= StepsPerIncrement; i++) {
+    digitalWrite(pulse, HIGH);
+    delay(10);
+    digitalWrite(pulse, LOW);
+    delay(10);
+  }
   
   //-------Logic for Selector Interrupt
   if(sel1flg != 0) //if the beam was not tripped, the pellet did not exit the selector
@@ -123,34 +138,54 @@ void loop() {
     step1[i] = step0[i];
   }
 
-  CogError(scale,step4)
+  CogError(scale,step4);
 
   //------
-}
+}//End of Main Loop
 
 
-//Servo Control Function
+//------Servo Control Function
 float servo_control(int servo){
-  int ScaleVal = analogRead(potpin1);  //Reads value of potentiometer (value between 0 and 1023)
-  val = map(ScaleVal,0,1023,0,90); //Converts 0-1023 range to 0-90
-  if ((val >=0) && (val <18)){  
+  float val = ScaleRead(servo);  //Reads value of scale
+  if ((val < VeryLow)){  
     pwm.writeMicroseconds(servo, 0*(USMAX-USMIN)/4+USMIN);    
   }
-  if ((val >=18) && (val <36)){
+  if ((val >= VeryLow) && (val < Low)){
     pwm.writeMicroseconds(servo, 1*(USMAX-USMIN)/4+USMIN);
   }
-  if ((val >=36) && (val <54)){
+  if ((val >= Low) && (val < High)){
     pwm.writeMicroseconds(servo, 2*(USMAX-USMIN)/4+USMIN);
   }
-  if ((val >=54) && (val <72)){
+  if ((val >= High) && (val < VeryHigh)){
     pwm.writeMicroseconds(servo, 3*(USMAX-USMIN)/4+USMIN);
   }
-  if ((val >=72) && (val <=90)){
+  if ((val >= VeryHigh)){
     pwm.writeMicroseconds(servo, 4*(USMAX-USMIN)/4+USMIN);
   }
-  return ScaleVal;  
+  return val;  
 }
 
+//------Load Cell Reading Function
+float ScaleRead(int scale) {
+  float ScaleVal = 0;
+  float sum = 0;
+  
+  for (int i = 0; i<=scaleTime; i++) {
+    ScaleVal = analogRead(scale);  // analog in 0 for Strain 1
+    sum = sum + ScaleVal;
+    delay(1);
+  }
+
+  ScaleVal = sum/scaleTime;
+  Serial.print(ScaleVal);
+  Serial.print("\t");
+  
+  float weight = ((LoadB_Strain[scale] - LoadA_Strain[scale])/(ReadingB_Strain[scale] - ReadingA_Strain[scale])) * (ScaleVal - ReadingA_Strain[scale]) + LoadA_Strain[scale];
+
+  Serial.print(weight);
+  Serial.print("\n");
+  return weight;
+}
 
 //------Cogwheel Error Check
 void CogError(float scale[9],bool stepLast[9]) {
