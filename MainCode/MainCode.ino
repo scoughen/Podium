020e529734304d7ddfc68676ff178d0b6537d4f5
scoughen <60477261+scoughen@users.noreply.github.com>
2020-03-27 13:30:02 -0600

    Update MainCode.ino

--- MainCode/MainCode.ino
+++ MainCode/MainCode.ino
@@ -17,7 +17,7 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define USMAX  1500 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
-float scale[9];  //values for current readings from all scales
+float scale[10];  //values for current readings from all scales
 
 //--------Stepper Motor Var's
 #define StepsPerIncrement 24
@@ -26,10 +26,10 @@ int dir = 6; // direction pin
 int en = 5; // enable pin
 
 //--------Scale Calibration Var's
-float ReadingA_Strain[9];
-float LoadA_Strain[9]; 
-float ReadingB_Strain[9];
-float LoadB_Strain[9];
+float ReadingA_Strain[10];
+float LoadA_Strain[10]; 
+float ReadingB_Strain[10];
+float LoadB_Strain[10];
 
 //--------Selector Interrupt Var's
 const byte SelectorInterruptPin = 2;
@@ -41,12 +41,28 @@ int funnelState = 0;
 
 //--------Cogwheel Error Checking Var's
 const byte inlet0 = 15; //inlet sensor pin
+const byte inlet1 = 16; //inlet sensor pin
+const byte inlet2 = 17; //inlet sensor pin
+const byte inlet3 = 18; //inlet sensor pin
+const byte inlet4 = 19; //inlet sensor pin
+const byte inlet5 = 20; //inlet sensor pin          <--  need to finalize ports
+const byte inlet6 = 21; //inlet sensor pin
+const byte inlet7 = 22; //inlet sensor pin
+const byte inlet8 = 23; //inlet sensor pin
+const byte inlet9 = 24; //inlet sensor pin
 const byte scale0 = A0; //scale pin
-bool step0[9]; //1 element per line  -- value for pellet presence before cogwheel
-bool step1[9]; //1 element per line  -- value for pellet presence when reached cogwheel
-bool step2[9]; //1 element per line  -- value for pellet presence 1 cycle after reaching cogwheel
-bool step3[9]; //1 element per line  -- value for pellet presence 2 cycles after reaching cogwheel
-bool step4[9]; //1 element per line  -- value for pellet presence when reaches scale
+bool step0[10]; //1 element per line  -- value for pellet presence before cogwheel
+bool step1[10]; //1 element per line  -- value for pellet presence when reached cogwheel
+bool step2[10]; //1 element per line  -- value for pellet presence 1 cycle after reaching cogwheel
+bool step3[10]; //1 element per line  -- value for pellet presence 2 cycles after reaching cogwheel
+bool step4[10]; //1 element per line  -- value for pellet presence 3 cycles after reaching cogwheel
+bool step5[10]; //1 element per line  -- value for pellet presence 4 cycles after reaching cogwheel
+bool step6[10]; //1 element per line  -- value for pellet presence 5 cycles after reaching cogwheel
+bool step7[10]; //1 element per line  -- value for pellet presence 6 cycles after reaching cogwheel
+bool step8[10]; //1 element per line  -- value for pellet presence 7 cycles after reaching cogwheel
+bool step9[10]; //1 element per line  -- value for pellet presence 8 cycles after reaching cogwheel
+bool step10[10]; //1 element per line  -- value for pellet presence 9 cycles after reaching cogwheel
+bool step11[10]; //1 element per line  -- value for pellet presence when reaches scale
 
 //--------
 
@@ -95,9 +111,12 @@ void setup() {
 
 
 void loop() {
+  
+  ScaleRead(); //Read all scales (values stored in "scale")
+ 
   for (int i = 0; i<=0; i++)
   {
-    scale[i] = servo_control(i,ScaleRead());
+    servo_control(i,scale[i]); //Set servos
   }
 
   //-------Cogwheel Control
@@ -128,25 +147,40 @@ void loop() {
   }
   
   //------Logic for Cogwheel Error Checking
-  step0[0] = digitalRead(inlet0);
-  
   for (int i = 0; i<= 9; i++) //transfer pellet presence data down the line
   {
+    step11[i] = step10[i];
+    step10[i] = step9[i];
+    step9[i] = step8[i];
+    step8[i] = step7[i];
+    step7[i] = step6[i];
+    step6[i] = step5[i];
+    step5[i] = step4[i];
     step4[i] = step3[i];
     step3[i] = step2[i];
     step2[i] = step1[i];
     step1[i] = step0[i];
   }
 
-  CogError(scale,step4);
+  step0[0] = digitalRead(inlet0);
+  step0[1] = digitalRead(inlet1);
+  step0[2] = digitalRead(inlet2);
+  step0[3] = digitalRead(inlet3);
+  step0[4] = digitalRead(inlet4);
+  step0[5] = digitalRead(inlet5);
+  step0[6] = digitalRead(inlet6);
+  step0[7] = digitalRead(inlet7);
+  step0[8] = digitalRead(inlet8);
+  step0[9] = digitalRead(inlet9);
+  
+  CogError(scale,step11);
 
   //------
 }//End of Main Loop
 
 
 //------Servo Control Function
-float servo_control(int servo, float val){
-  float val = ScaleRead(servo);  //Reads value of scale
+void servo_control(int servo, float val){
   if ((val < VeryLow)){  
     pwm.writeMicroseconds(servo, 0*(USMAX-USMIN)/4+USMIN);    
   }
@@ -161,30 +195,36 @@ float servo_control(int servo, float val){
   }
   if ((val >= VeryHigh)){
     pwm.writeMicroseconds(servo, 4*(USMAX-USMIN)/4+USMIN);
-  }
-  return val;  
+  } 
+  
+  return;
 }
 
 //------Load Cell Reading Function
-float ScaleRead() {
-  float ScaleVal = 0;
-  float sum = 0;
+void ScaleRead() {
+  float ScaleVal[10] = {0,0,0,0,0,0,0,0,0,0};
+  float sum[10] = {0,0,0,0,0,0,0,0,0,0};
   
   for (int i = 0; i<=scaleTime; i++) {
-    ScaleVal = analogRead(scale);  // analog in 0 for Strain 1
-    sum = sum + ScaleVal;
+    for (int a = 0; a <= 9; a++){
+      ScaleVal[a] = analogRead(a);  // analog in 0 for Strain 1
+      sum[a] = sum[a] + ScaleVal[a];
+    }
     delay(1);
   }
 
-  ScaleVal = sum/scaleTime;
-  Serial.print(ScaleVal);
-  Serial.print("\t");
+  for (int a = 0; a <= 9; a++) {
+    ScaleVal[a] = sum[a]/scaleTime;
+    Serial.print(ScaleVal[a]);
+    Serial.print("\t");
+    
+    scale[a] = ((LoadB_Strain[a] - LoadA_Strain[a])/(ReadingB_Strain[a] - ReadingA_Strain[a])) * (ScaleVal[a] - ReadingA_Strain[a]) + LoadA_Strain[a];
   
-  float weight = ((LoadB_Strain[scale] - LoadA_Strain[scale])/(ReadingB_Strain[scale] - ReadingA_Strain[scale])) * (ScaleVal - ReadingA_Strain[scale]) + LoadA_Strain[scale];
-
-  Serial.print(weight);
-  Serial.print("\n");
-  return weight;
+    Serial.print(scale[a]);
+    Serial.print("\n");
+  }
+  
+  return;
 }
 
 //------Cogwheel Error Check
