9bfc238df38cf853431dbce8957966641c1fc797
scoughen <60477261+scoughen@users.noreply.github.com>
2020-04-25 17:07:52 -0600

    Update MainCode.ino

--- MainCode/MainCode.ino
+++ MainCode/MainCode.ino
@@ -8,7 +8,6 @@
     #define Low 0.525 // (g)
     #define High 0.535 // (g)
     #define VeryHigh 0.550 // (g)
-  //Bin Contents Threshold
     #define binMax 100000 // maximum number of pellets that can enter a bin before stoping the process
 //------END OF USER CONTROLLED VALUES
 
@@ -17,6 +16,7 @@
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 Ard2499 adc;
 
+//--------Servo Settings
 #define USMIN  1000 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
 #define USMAX  1500 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
@@ -24,7 +24,7 @@ Ard2499 adc;
 float scale[10];  //values for current readings from all scales
 
 //--------Stepper Motor Var's
-#define StepsPerIncrement 20
+#define StepsPerIncrement 10*4 //   <-- assumes microstepping with 4 microsteps per whole step
 int pulse = 7; //pulse pin
 int dir = 6; // direction pin
 int en = 5; // enable pin
@@ -43,8 +43,15 @@ int HighCount = 0;
 int VeryHighCount = 0;
 
 //--------Selector Interrupt Var's
-const byte SelectorInterruptPin = 2;
-int sel1flg = 0;
+const byte SelectorInterruptPin0 = 2;
+const byte SelectorInterruptPin1 = 3;
+const byte SelectorInterruptPin2 = 18;
+const byte SelectorInterruptPin3 = 19;
+const byte SelectorInterruptPin4 = 20;
+const byte SelectorInterruptPin5 = 21;
+//This allows for 6 channels to have the selector throughput monitored
+//If monitoring the rest of the channels is desired, pin change interrupts will be needed
+int sel1flg[6] = {0,0,0,0,0,0};
 
 //--------Cogwheel Error Checking Var's
 const byte inlet0 = 15; //inlet sensor pin
@@ -78,19 +85,60 @@ int ErrorCount = 10;
 
 //--------
 
+
 void setup() {
   //--------Scale Calibration Data
     //--------Scale 0
-      ReadingA_Strain[0] = 29965866.00;
-      LoadA_Strain[0] = 1.0; //  (g) 
-      ReadingB_Strain[0] = 30139296.00;
-      LoadB_Strain[0] = 2.0; //  (g) 
+      ReadingA_Strain[0] = 29965866.00; //       <------------input scale raw reading for following weight here
+      LoadA_Strain[0] = 1.0; //  (g) <-- calibration weight
+      ReadingB_Strain[0] = 30139296.00; //       <------------input scale raw reading for following weight here
+      LoadB_Strain[0] = 2.0; //  (g) <-- calibration weight
     //--------Scale 1
       ReadingA_Strain[1] = 866.0;
-      LoadA_Strain[1] = 0.0; //  (g) 
+      LoadA_Strain[1] = 1.0; //  (g) 
       ReadingB_Strain[1] = 947.0;
-      LoadB_Strain[1] = 2.9; //  (g) 
-    //--------Scale etc.......................................
+      LoadB_Strain[1] = 2.0; //  (g) 
+    //--------Scale 2
+      ReadingA_Strain[2] = 866.0;
+      LoadA_Strain[2] = 1.0; //  (g) 
+      ReadingB_Strain[2] = 947.0;
+      LoadB_Strain[2] = 2.0; //  (g) 
+    //--------Scale 3
+      ReadingA_Strain[3] = 866.0;
+      LoadA_Strain[3] = 1.0; //  (g) 
+      ReadingB_Strain[3] = 947.0;
+      LoadB_Strain[3] = 2.0; //  (g) 
+    //--------Scale 4
+      ReadingA_Strain[4] = 866.0;
+      LoadA_Strain[4] = 1.0; //  (g) 
+      ReadingB_Strain[4] = 947.0;
+      LoadB_Strain[4] = 2.0; //  (g) 
+    //--------Scale 5
+      ReadingA_Strain[5] = 866.0;
+      LoadA_Strain[5] = 1.0; //  (g) 
+      ReadingB_Strain[5] = 947.0;
+      LoadB_Strain[5] = 2.0; //  (g) 
+    //--------Scale 6
+      ReadingA_Strain[6] = 866.0;
+      LoadA_Strain[6] = 1.0; //  (g) 
+      ReadingB_Strain[6] = 947.0;
+      LoadB_Strain[6] = 2.0; //  (g) 
+    //--------Scale 7
+      ReadingA_Strain[7] = 866.0;
+      LoadA_Strain[7] = 1.0; //  (g) 
+      ReadingB_Strain[7] = 947.0;
+      LoadB_Strain[7] = 2.0; //  (g) 
+    //--------Scale 8
+      ReadingA_Strain[8] = 866.0;
+      LoadA_Strain[8] = 1.0; //  (g) 
+      ReadingB_Strain[8] = 947.0;
+      LoadB_Strain[8] = 2.0; //  (g) 
+    //--------Scale 9
+      ReadingA_Strain[9] = 866.0;
+      LoadA_Strain[9] = 1.0; //  (g) 
+      ReadingB_Strain[9] = 947.0;
+      LoadB_Strain[9] = 2.0; //  (g) 
+    
 
   //------Servo setup
   Serial.begin(9600);
@@ -99,7 +147,7 @@ void setup() {
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
   delay(10);
 
-  //------Set up for Stepper Motor
+  //------Set up for Stepper Motor Controller
   pinMode (pulse, OUTPUT);
   pinMode (dir, OUTPUT);
   pinMode (en, OUTPUT);
@@ -111,14 +159,41 @@ void setup() {
   adc.ltc2499ChangeChannel(LTC2499_CHAN_SINGLE_0P);
 
   //------Set up for Selector Interrupt
-  pinMode(SelectorInterruptPin, INPUT_PULLUP);
-  int sel1flg;
+  pinMode(SelectorInterruptPin0, INPUT_PULLUP);
+  pinMode(SelectorInterruptPin1, INPUT_PULLUP);
+  pinMode(SelectorInterruptPin2, INPUT_PULLUP);
+  pinMode(SelectorInterruptPin3, INPUT_PULLUP);
+  pinMode(SelectorInterruptPin4, INPUT_PULLUP);
+  pinMode(SelectorInterruptPin5, INPUT_PULLUP);
   
-  attachInterrupt(digitalPinToInterrupt(SelectorInterruptPin), Selector, RISING);
+  attachInterrupt(digitalPinToInterrupt(SelectorInterruptPin0), Selector0, FALLING);
+  attachInterrupt(digitalPinToInterrupt(SelectorInterruptPin1), Selector1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(SelectorInterruptPin2), Selector2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(SelectorInterruptPin3), Selector3, FALLING);
+  attachInterrupt(digitalPinToInterrupt(SelectorInterruptPin4), Selector4, FALLING);
+  attachInterrupt(digitalPinToInterrupt(SelectorInterruptPin5), Selector5, FALLING);
   
   //------Set up for Cogwheel Error Checking
   pinMode(inlet0, INPUT);
   pinMode(scale0, INPUT);
+  pinMode(inlet1, INPUT);
+  pinMode(scale1, INPUT);
+  pinMode(inlet2, INPUT);
+  pinMode(scale2, INPUT);
+  pinMode(inlet3, INPUT);
+  pinMode(scale3, INPUT);
+  pinMode(inlet4, INPUT);
+  pinMode(scale4, INPUT);
+  pinMode(inlet5, INPUT);
+  pinMode(scale5, INPUT);
+  pinMode(inlet6, INPUT);
+  pinMode(scale6, INPUT);
+  pinMode(inlet7, INPUT);
+  pinMode(scale7, INPUT);
+  pinMode(inlet8, INPUT);
+  pinMode(scale8, INPUT);
+  pinMode(inlet9, INPUT);
+  pinMode(scale9, INPUT);
 }
 
 
@@ -131,30 +206,86 @@ void loop() {
     servo_control(i,scale[i]); //Set servos
   }
 
-  //-------Cogwheel Control
+     //-------Cogwheel Control
   digitalWrite(dir, LOW);
   digitalWrite(en, HIGH);
   
-  for (int i=0; i <= StepsPerIncrement; i++) {
+  for (int i=0; i <= StepsPerIncrement; i++) {  //each pair of high/low digital writes causese 1 step
     digitalWrite(pulse, HIGH);
-    delay(4);
     digitalWrite(pulse, LOW);
-    delay(4);
+    delay(6);
+    if (i >= StepsPerIncrement-12) {
+      switch (StepsPerIncrement-i) {
+        case 0:
+          delay(6);
+        break;
+        case 1:
+          delay(6);
+        break;
+        case 2:
+          delay(6);
+        break;
+        case 3:
+          delay(5);
+        break;
+        case 4:
+          delay(5);
+        break;
+        case 5:
+          delay(4);
+        break;
+        case 6:
+          delay(4);
+        break;
+        case 7:
+          delay(3);
+        break;
+        case 8:
+          delay(3);
+        break;
+        case 9:
+          delay(2);
+        break;
+        case 10:
+          delay(2);
+        break;
+        case 11:
+          delay(1);
+        break;
+        case 12:
+          delay(1);
+        break;
+      }
+    }
+  }
+  delay(200);
+  digitalWrite(dir, HIGH);
+  for (int i=0; i <= 2*4; i++) {  //      <-- this pair of for loops does a short "back and forth" motion of the cog wheel to help with making sure the pellet entered the hole
+    digitalWrite(pulse, HIGH);
+    digitalWrite(pulse, LOW);
+    delay(16);
+  }
+  delay(200);
+  digitalWrite(dir, LOW);
+  for (int i=0; i <= 1.25*4; i++) {
+    digitalWrite(pulse, HIGH);
+    digitalWrite(pulse, LOW);
+    delay(16);
   }
   
   //-------Logic for Checking Bin Contents
   if((VeryLowCount >= binMax)||(LowCount >= binMax)||(GoodCount >= binMax)||(HighCount >= binMax)||(VeryHighCount>= binMax))
   {
-    Bin_fill_shutdown;
+    Bin_fill_shutdown;  //If the number of pellets in any bin exceeds the max number allowed, stop the machine
   }
   
   //-------Logic for Selector Interrupt
-  if(sel1flg != 0) //if the beam was not tripped, the pellet did not exit the selector
+  if((sel1flg[0] != 1) || (sel1flg[1] != 1) || (sel1flg[2] != 1) || (sel1flg[3] != 1) || (sel1flg[4] != 1) \\ (sel1flg[5] != 1)) //if the beam was not tripped, the pellet did not exit the selector
   {
     //Problem between scale and selector exit!!!!
    Error++;
    if (Error = ErrorCount) {
-//    Selector_shutdown();
+    Selector_shutdown();
    }
   }
   else //if the beam was tripped, the pellet exited the selector
@@ -190,7 +321,7 @@ void loop() {
   step0[8] = digitalRead(inlet8);
   step0[9] = digitalRead(inlet9);
   
-  CogError(scale,step11);
+  CogError(scale,step11);  //if there is an issue due to missmatching, this function will stop the machine
 
   //------Bin Volume Output
   if ((VeryLowCount%100 == 0)||(LowCount%100 == 0)||(GoodCount%100 == 0)||(HighCount%100 == 0)||(VeryHighCount%100 == 0))
@@ -201,7 +332,7 @@ void loop() {
     Serial.print("LowCount = ");
     Serial.print(LowCount);
     Serial.print("\t");
-    Serial.print("GoodCount = ");
+    Serial.print("GoodCount = ");  //      counting the number of pellets sent to each bin
     Serial.print(GoodCount);
     Serial.print("\t");
     Serial.print("HighCount = ");
@@ -216,7 +347,7 @@ void loop() {
 
 
 //------Servo Control Function
-void servo_control(int servo, float val){
+void servo_control(int servo, float val){  //positions selector servo based on the user set weight thresholds and the weight of the current pellet
   if ((val < VeryLow)){  
     pwm.writeMicroseconds(servo, 0*(USMAX-USMIN)/4+USMIN);    
     VeryLowCount++; // Increment count of pellets in Very Low Bin   
@@ -319,15 +450,30 @@ void CogError(float scale[10],bool stepLast[10]) {
       //Issue between system inlet and scale!!!!
       CogErrorFlg[i]++;
       if (CogErrorFlg[i] == ErrorCount) {
-//        Cog_shutdown();
+        Cog_shutdown();
       }
     }
   }
 }
 
 //------Selector ISR
-void Selector() {
-  sel1flg = 1; //set flag when pellet exits selector
+void Selector0() {
+  sel1flg[0] = 1; //set flag when pellet exits selector
+}
+void Selector1() {
+  sel1flg[1] = 1; //set flag when pellet exits selector
+}
+void Selector2() {
+  sel1flg[2] = 1; //set flag when pellet exits selector
+}
+void Selector3() {
+  sel1flg[3] = 1; //set flag when pellet exits selector
+}
+void Selector4() {
+  sel1flg[4] = 1; //set flag when pellet exits selector
+}
+void Selector5() {
+  sel1flg[5] = 1; //set flag when pellet exits selector
 }
 
 //------Cogwheel Shutdown
